06c6a4856733540bc127b9117fe51b5d77f0314a
ftrajano <ftrajano@gmail.com>
2023-06-16 13:25:12 -0300

    finish deleteNode js

--- javascript/LinkedList.js
+++ javascript/LinkedList.js
@@ -54,15 +54,68 @@ class LinkedList{
         }
     }
 
+    insertAtIndex(value, position){
+        if(this._length+1<position){
+            console.log('Lista de tamanho menor do que a posicao!')
+        } else {
+
+            const newNode = new Node(value);
+            let curr = this._head;
+            for(let i=0;i<position-2;i++){
+                curr = curr._next;
+            }
+            newNode._next = curr._next;
+            curr._next = newNode;
+        }
+
+    }
+
+    deleteNode(value){
+        let curr = this._head;
+        if(curr.value ===value){
+            if(this._length===1){
+                this._head = null;
+                this._tail =null;
+                this._length--;
+            } else {
+                this._head= this._head._next
+            }
+        } else {
+            while(curr._next!==null){
+                if(curr._next._value===value){
+                    curr._next = curr._next._next
+                    break;
+                } else {
+                    curr=curr._next;
+                }
+            console.log('Valor não encontrado!')
+            }
+        }
+    }
+
+
     printList(){
         let lista = '';
         let aux = this._head;
-        while(aux._next!=null){
-            lista+=aux._value+' ';
-            aux = aux._next; 
+        if(this._length===0){
+            console.log('lista vazia')
+        } else {
+            while(aux._next!=null){
+                lista+=aux._value+' ';
+                aux = aux._next; 
+            }
+            lista+=aux._value;
+            console.log(lista)
         }
-        lista+=aux._value;
-        console.log(lista)
+        
+    }
+
+    deleteAtBeginning(){
+
+    }
+
+    deleteAtEnd(){
+        
     }
 }
 
--- javascript/main.js
+++ javascript/main.js
@@ -12,3 +12,11 @@ l1.insertAtEnd(10);
 l1.clear();
 l1.insertAtBeginning(8);
 l1.printList();
+l1.insertAtIndex(10,3)
+l1.printList();
+l1.insertAtIndex(10,2)
+l1.printList()
+l1.deleteNode(10)
+l1.printList()
+l1.deleteNode(8)
+l1.printList()
