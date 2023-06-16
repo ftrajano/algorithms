872eead7c70b2da48e0abe5849e99dc2a36e645f
ftrajano <ftrajano@gmail.com>
2023-06-16 17:05:27 -0300

    all javascript linked lists implemented; missing tests of the last 3(in the comments)

--- javascript/LinkedList.js
+++ javascript/LinkedList.js
@@ -1,5 +1,22 @@
 const Node =require('./Node.js');
 
+// Operações que vamos implantar nas linked lists:
+
+// head - Returns the head node of the LinkedList. ok
+// tail - Returns the tail node of the LinkedList. ok
+// length - Returns the length (number of nodes) in the LinkedList. ok 
+// clear - Clears the LinkedList by removing all nodes. ok
+// insertAtBeginning - Inserts a new node with the given value at the beginning of the LinkedList. ok
+// insertAtEnd - Inserts a new node with the given value at the end of the LinkedList. ok
+// insertAtIndex - Inserts a new node with the given value at the specified index in the LinkedList. ok
+// deleteNode - Deletes the node with the given value from the LinkedList. ok
+// deleteAtBeginning - Deletes the first node from the LinkedList. ok Falta testar
+// deleteAtEnd - Deletes the last node from the LinkedList. ok Falta testar
+// searchValue - Searches for the given value in the LinkedList and returns it if found. ok Falta testar
+// isEmpty - Checks if the LinkedList is empty and returns a boolean value. ok
+// printList - Prints the values of all nodes in the LinkedList. ok
+
+
 class LinkedList{
 
     constructor(value){
@@ -111,11 +128,57 @@ class LinkedList{
     }
 
     deleteAtBeginning(){
+        if(this._length===0){
+            console.log('Lista vazia!')
+        } else if(this._length==1){
+            this._head=null;
+            this._tail=null;
+            this._length=0;
+        } else {
+            this._head=this._head.next;
+            this._length--;
+        }
+    }
+    
+
+    deleteAtEnd(){
+        if(this._length===0){
+            console.log('Lista vazia!')
+        } else if(this._length==1){
+            this._head=null;
+            this._tail=null;
+            this._length=0;
+        } else {
+            let curr = this._head
+            for(let i = 1; i<this._length-1< i++){
+                curr=curr.next;
+            }
+            this._tail = curr;
+            curr._next= null;
+            this._length--;
+        }
 
     }
 
-    deleteAtEnd(){
-        
+    searchValue(value){
+        let curr = this._head;
+        if(this._length===0){
+            console.log('empty list!');
+        } else {
+            while(curr._value!==value){
+                if(curr._next!==null){
+                    console.log('value not found!');
+                } else {
+                    curr=curr._next;
+                }
+            }
+        }
+    }
+
+    isEmpty(){
+        if(this._length===0){
+            return true;
+        }
     }
 }
 
